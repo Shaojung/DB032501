579bd8550aedcb94ee899e00168d8c3ae8daf8a2
Shaojung <shaojung@gmail.com>
2017-04-01 12:01:25 +0800

    add DetailActivity

--- app/src/main/java/com/example/teacher/db032501/DetailActivity.java
+++ app/src/main/java/com/example/teacher/db032501/DetailActivity.java
@@ -2,12 +2,13 @@ package com.example.teacher.db032501;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.teacher.db032501.data.Student;
 import com.example.teacher.db032501.data.StudentDAOMemoryImpl;
 
 public class DetailActivity extends AppCompatActivity {
-
+    TextView tv1, tv2, tv3, tv4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,7 +16,14 @@ public class DetailActivity extends AppCompatActivity {
         int ID = getIntent().getIntExtra("ID", 0);
         StudentDAOMemoryImpl impl = new StudentDAOMemoryImpl();
         Student student = impl.getStudent(ID);
-
+        tv1 = (TextView) findViewById(R.id.tvDetailID);
+        tv2 = (TextView) findViewById(R.id.tvDetailName);
+        tv3 = (TextView) findViewById(R.id.tvDetailAddr);
+        tv4 = (TextView) findViewById(R.id.tvDetailTel);
+        tv1.setText(String.valueOf(student.ID));
+        tv2.setText(student.Name);
+        tv3.setText(student.Addr);
+        tv4.setText(student.Tel);
 
     }
 }
